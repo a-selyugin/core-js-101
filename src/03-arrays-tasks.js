2ce8141c2b22f6d4715bb06c79a489f6e1721d12
a-selyugin <selyuginai@gmail.com>
2022-07-22 12:10:49 +0300

    feat: started 04 module

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -548,8 +548,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.flatMap(childrenSelector);
 }
 
 
@@ -565,8 +565,8 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((acc, curr) => acc[curr], arr);
 }
 
 
@@ -588,8 +588,14 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const tail = arr.slice(Math.ceil(arr.length / 2));
+  const head = arr.slice(0, Math.floor(arr.length / 2));
+  const middle = [];
+  if (arr.length % 2) {
+    middle.push(arr[Math.floor(arr.length / 2)]);
+  }
+  return tail.concat(middle).concat(head);
 }
 
 
